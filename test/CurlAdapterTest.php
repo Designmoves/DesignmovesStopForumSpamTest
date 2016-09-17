09fb21d0edd0131a1384bc44e9dd847bece9ca71
Designmoves <github@designmoves.nl>
2016-09-18 01:41:29 +0200

    Added dummy test

---
+++ test/CurlAdapterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DesignmovesStopForumSpamTest;
+
+use PHPUnit_Framework_TestCase;
+
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertSame(true, true);
+    }
+}
