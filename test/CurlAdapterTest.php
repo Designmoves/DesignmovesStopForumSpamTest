10d153de03d7848adb74888c47a6dc350e691168
Designmoves <github@designmoves.nl>
2016-09-18 02:27:59 +0200

    Added ZF2 Curl test

--- test/CurlAdapterTest.php
+++ test/CurlAdapterTest.php
@@ -3,11 +3,35 @@
 namespace DesignmovesStopForumSpamTest;
 
 use PHPUnit_Framework_TestCase;
+use Zend\Http\Client as ZendHttpClient;
 
 class ResponseTest extends PHPUnit_Framework_TestCase
 {
-    public function testTrue()
+    /**
+     * @var ZendHttpClient
+     */
+    protected $httpClient;
+  
+    /**
+     * @dataProvider providerUrls
+     */
+    public function testHttpClientWithoutCurlOptions($uri)
     {
-        $this->assertSame(true, true);
+        $config = [
+            'adapter' => 'Zend\Http\Client\Adapter\Curl',
+        ];
+        $this->client = new ZendHttpClient($uri, $config);
+        $this->client->send();
+
+        $response = $this->client->getResponse();
+
+        $this->assertContains('<root>', $response->getContent());
+    }
+    
+    public function providerUrls()
+    {
+        return [
+            ['http://www.stopforumspam.com/api?email=&ip=127.0.0.1&f=xmldom'],
+        ];
     }
 }
